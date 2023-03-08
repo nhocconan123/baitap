f45a2c78a7d343c9fc05d8d4c759d83da601e64a
nhocconan123 <nhocconan008@gmail.com>
2023-03-08 16:31:37 +0700

    baitap3

--- src/main/java/bai3/main.java
+++ src/main/java/bai3/main.java
@@ -10,7 +10,7 @@ public class main {
         VanBan vb=new VanBan(noidung);
         System.out.println("so tu cua chuoi la: ");
         vb.demsoluongchu();
-        System.out.println(" chuan hoa xau khong co ky tu trang o giau xau");
+        System.out.println(" chuan hoa xau khong co ky tu trang");
         vb.chuanhoaxau();
         System.out.println("chuan hoa van ban thanh chu viet hoa");
         vb.hoaToUpperCase();
